e2e308b33bb35629365c1da521a26ba7b218afbd
James Ferguson <ncjamesferguson@gmail.com>
2018-07-16 22:36:54 +0000

    automatically backed up by learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,25 +3,20 @@ import React from 'react';
 class Filters extends React.Component {
   constructor() {
     super();
-
-    this.props = {type: ""}
   }
 
-  handleChange = (event) => {
-    this.props.onChangeType(event.target.value)
-  }
-
-  handleFindPets = () => {
-    this.props.onFindPetsClick()
-  }
+  handleChangeType = event => this.props.onChangeType(event.target.value);
 
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-
-          <select name="type" id="type" value={this.props.filters.type} onChange={this.handleChange}>
+          <select
+          name="type"
+          id="type"
+          value={this.props.filters.type}
+          onChange={this.handleChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -30,7 +25,9 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button" onClick={this.handleFindPets}>Find pets</button>
+          <button
+          className="ui secondary button"
+          onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
